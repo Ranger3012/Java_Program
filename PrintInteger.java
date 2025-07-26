f7d92e587259e0836b5ec1713cd762ec55493260
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2025-07-26 01:50:24 -0400

    print integer program created.

---
+++ PrintInteger.java
@@ -0,0 +1,10 @@
+class PrintInteger{
+    public static void main(String[] args) {
+        int number; // Declaring a variable
+        
+        //Use loop to print integers
+        for(number = 1; number <= 10; number++){
+            System.out.println(number);
+        }
+    }
+}
