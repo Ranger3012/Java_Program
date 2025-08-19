4844e559f384c76693155d4fac0acc002516c96d
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2025-08-19 00:19:34 -0400

    created EnhancedLoop programs.

---
+++ EnhancedLoop2.java
@@ -0,0 +1,9 @@
+class EnhancedLoop2{
+    public static void main(String[] args) {
+        String languages[] = {"Java", "C++", "Python"};
+
+        for(String strong : languages){
+            System.out.println(strong);
+        }
+    }
+}
